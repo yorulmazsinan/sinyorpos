202d3aa1f1422c7bf7bd71142a0e6ca5c45eb5b9
Sinan Yorulmaz <info@sinanyorulmaz.com>
2023-09-05 22:54:53 +0300

    Yapı Kredi ile ilgili yeni POS dosyaları eklendi.

---
+++ src/DataMapper/ResponseDataMapper/VakifBankCPPosResponseDataMapper.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EceoPos\DataMapper\ResponseDataMapper;
+
+/**
+ * @deprecated use PayFlexCPV4PosResponseDataMapper instead
+ */
+class VakifBankCPPosResponseDataMapper extends PayFlexCPV4PosResponseDataMapper
+{
+}
---
+++ src/DataMapper/ResponseDataMapper/VakifBankPosResponseDataMapper.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EceoPos\DataMapper\ResponseDataMapper;
+
+/**
+ * @deprecated use PayFlexV4PosResponseDataMapper instead
+ */
+class VakifBankPosResponseDataMapper extends PayFlexV4PosResponseDataMapper
+{
+}
