f53d52534378c4e9522dd4d402ec8f104cbaab66
Sinan Yorulmaz <info@sinanyorulmaz.com>
2023-09-07 23:28:56 +0300

    "posNet" sınıfı güncellendi.

--- src/Gateways/PosNet.php
+++ src/Gateways/PosNet.php
@@ -130,11 +130,9 @@ class PosNet extends AbstractGateway
             throw new LogicException('Kredi kartı veya sipariş bilgileri eksik!');
         }
 
-        $data = $this->getOosTransactionData();
-
         $this->logger->log(LogLevel::DEBUG, 'preparing 3D form data');
 
-        return $this->requestDataMapper->create3DFormData($this->account, $this->order, $this->type, $this->get3DGatewayURL(), $this->card, $data['oosRequestDataResponse']);
+        return $this->requestDataMapper->create3DFormData($this->account, $this->order, $this->type, $this->get3DGatewayURL(), $this->card);
     }
 
     /**
